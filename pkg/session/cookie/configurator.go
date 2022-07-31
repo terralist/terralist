d48c05d63afd8182678dd1f243d2b25a0d472b00
Valentin Deaconu <valentin.l.deaconu@gmail.com>
2022-07-31 17:27:21 +0300

    Add impl for cookie sessions

---
+++ pkg/session/cookie/configurator.go
@@ -0,0 +1,25 @@
+package cookie
+
+import "fmt"
+
+// Config implements session.Configurator interface
+// and handles the configuration parameters of the
+// cookie session implementation
+type Config struct {
+	Name   string
+	Secret string
+}
+
+func (c *Config) SetDefaults() {
+	if c.Name == "" {
+		c.Name = "_session"
+	}
+}
+
+func (c *Config) Validate() error {
+	if c.Secret == "" {
+		return fmt.Errorf("missing required attribute 'Secret'")
+	}
+
+	return nil
+}
---
+++ pkg/session/cookie/session.go
@@ -0,0 +1,21 @@
+package cookie
+
+import "github.com/gorilla/sessions"
+
+// Session is a concrete implementation of session.Session
+// and is a wrapper over gorilla's session
+type Session struct {
+	session *sessions.Session
+}
+
+func (s *Session) Get(key any) (any, bool) {
+	if v, ok := s.session.Values[key]; ok {
+		return v, true
+	}
+
+	return nil, false
+}
+
+func (s *Session) Set(key any, value any) {
+	s.session.Values[key] = value
+}
