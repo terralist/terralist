e3d720f1435bab6bbf787620e1d46606c783dc08
Valentin Deaconu <valentin.l.deaconu@gmail.com>
2022-10-06 20:04:15 +0300

    Set token expire date only if expireIn is positive, non-zero

--- internal/server/services/api_key.go
+++ internal/server/services/api_key.go
@@ -64,7 +64,7 @@ func (s *DefaultApiKeyService) Grant(authorityID uuid.UUID, expireIn int) (strin
 		AuthorityID: authorityID,
 	}
 
-	if expireIn != 0 {
+	if expireIn > 0 {
 		exp := time.Now().Add(time.Duration(expireIn) * time.Hour)
 		apiKey.Expiration = &exp
 	}
